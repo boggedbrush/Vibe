16cd51a80fcf16c7d7dfc1d815427e74558b14e9
Justin Shaw <wyojustin@gmail.com>
2025-05-08 17:34:55 -0400

    pre-branch commit

--- tests/add_class_create/VibeBackups/hello_20250418_172219.py
+++
@@ -1,7 +0,0 @@
-# hello.py
-def greet(name):
-    print(f"Hello, {name}!")
-
-class Greeter:
-    def __init__(self, name):
-        print(f'Hello {name}')
--- tests/multi_patch/VibeBackups/hello_20250419_210438.py
+++
@@ -1,7 +0,0 @@
-# hello.py
-def greet(name):
-    print(f"Hello, {name}!")
-
-class Greeter:
-    def greet(self):
-        print("Greeter says hi!")
