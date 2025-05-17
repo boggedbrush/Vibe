20b93026123fd552f47846ae13b5e376f111a1bb
Justin Shaw <wyojustin@gmail.com>
2025-05-17 17:57:56 -0400

    Cleaned up, added new spellbook test

--- tests/add_route/hello.py
+++ tests/add_route/hello.py
@@ -2,7 +2,10 @@
 def greet(name):
     print(f"Hello, {name}!")
 
-
 class Greeter:
     def __init__(self, name):
-        print(f"Hello, {name}!")
+        print(f'Hello {name}')
+
+# Entry point
+if __name__ == '__main__':
+    print('here')
